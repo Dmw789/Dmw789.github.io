f0bf3217d0111b0cf15ad5af2efc490a888afa34
dmw789 <dmw171953@gmail.com>
2024-06-04 22:05:52 -0400

    Added github pages support

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'Dmw789.github.io', // Replace with your repository name
+  base: '/Dmw789.github.io/',
   plugins: [react()],
 });
