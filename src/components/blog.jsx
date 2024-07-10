35f04b911a7c12e166eb8dc893fa03ee8e48ca19
dmw789 <dmw171953@gmail.com>
2024-07-09 23:58:48 -0400

    updated routing

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -5,6 +5,14 @@ function Blog({posts}) {
     return(
         <div className={styles.coming_soon}>
             <h1>Blog - Coming Soon!</h1>
+            <iframe
+                src="https://yourblogname.blogspot.com/2024/07/your-post-title.html"
+                width="100%"
+                height="600px"
+                frameBorder="0"
+                scrolling="yes"
+                title="Blogger Post"
+            ></iframe>
         </div>
     )
 }
